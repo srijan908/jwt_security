3ac79130c1a3dd0e00209eb1aaff577a2b04dc50
srijan908 <59416123+srijan908@users.noreply.github.com>
2024-01-28 16:03:21 +0530

    Initial commit

---
+++ src/main/java/com/work/security/user/Role.java
@@ -0,0 +1,8 @@
+package com.work.security.user;
+
+public enum Role {
+
+    USER,
+
+    ADMIN
+}
---
+++ src/test/java/com/work/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
